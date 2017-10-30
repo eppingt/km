5ce65a9987745c4ae278a845c587546ebd4ddaa4
epping <31546009+eppingt@users.noreply.github.com>
2017-10-30 09:25:23 -0400

    Updated scripts order
    Updated the order of the scripts

--- computer rename.ps1
+++ computer rename.ps1
@@ -8,6 +8,14 @@ $name = Read-Host -Prompt "Please Enter the ComputerName you want to use."
 $computername.Rename($name)
 
 <#
+This Section of script prompts you to enter your current timezone
+#>
+
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null 
+$time = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Desired Time Zone")
+Set-TimeZone("$time")
+
+<#
 This section of script prompts you to enter the local time
 #>
 
@@ -15,14 +23,6 @@ This section of script prompts you to enter the local time
 $date = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Desired Time in MM/DD/YYYY HH:MM:SS AM/PM")
 Set-Date("$date")
 
-
-<#
-This Section of script prompts you to enter your current timezone
-#>
-
-[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null 
-$time = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Desired Time Zone")
-Set-TimeZone("$time")
 
 <#
 This section of script reboots the server.
