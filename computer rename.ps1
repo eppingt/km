8f0602147a139bee0936850f2adbf073972941a0
epping <31546009+eppingt@users.noreply.github.com>
2017-10-30 08:50:44 -0400

    Added date & timezone scripts
    Added scripts to set date/time and timezone

--- computer rename.ps1
+++ computer rename.ps1
@@ -8,7 +8,24 @@ $name = Read-Host -Prompt "Please Enter the ComputerName you want to use."
 $computername.Rename($name)
 
 <#
-This section of script reboots the newly renamed server.
+This section of script prompts you to enter the local time
+#>
+
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null 
+$date = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Desired Time in MM/DD/YYYY HH:MM:SS AM/PM")
+Set-Date("$date")
+
+
+<#
+This Section of script prompts you to enter your current timezone
+#>
+
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null 
+$time = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Desired Time Zone")
+Set-TimeZone("$time")
+
+<#
+This section of script reboots the server.
 #>
 
 Restart-Computer
